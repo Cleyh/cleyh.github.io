09317471b6216da34fd175019f0d1cff49a80300
Cleyh <unbd_233@qq.com>
2019-08-20 20:07:20 +0800

    message

---
+++ custom/languages/Chinese/installer.php
@@ -0,0 +1,91 @@
+<?php
+/*
+ *	Made by Samerton
+ *	Translation  by Hi_Michael
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr3
+ *
+ *  License: MIT
+ *
+ *  Chinese Language - Installation
+ *  Translation progress : 98%
+ *  翻译有误请使用GitHun回报issues 
+ *  https://github.com/haer0248/NamelessMC-v2-Traditional-Chinese/issues
+ */
+
+$language = array(
+    /*
+     *  Installation
+     */
+    'install' => '安装嚮导',
+    'pre-release' => '先行测试版',
+    'installer_welcome' => '欢迎使用 NamelessMC version 2.0 先行测试版.',
+    'pre-release_warning' => '请注意! 此版本不适用于公共场所.',
+    'installer_information' => '安装程序将指导您安装完成.',
+    'new_installation_question' => '首先，这是新的安装?',
+    'new_installation' => '新的安装 &raquo;',
+    'upgrading_from_v1' => '从 v1 升级 &raquo;',
+    'requirements' => '需要:',
+    'config_writable' => 'core/config.php 可写入',
+    'cache_writable' => 'Cache 可写入',
+    'template_cache_writable' => 'Template Cache 可写入',
+    'exif_imagetype_banners_disabled' => 'Without the exif_imagetype function, server banners will be disabled.',
+    'requirements_error' => '你必须安装所有必需的，并设置正确才能继续安装.',
+    'proceed' => '继续',
+    'database_configuration' => '资料库设定',
+    'database_address' => '资料库位置',
+    'database_port' => '资料库端口',
+    'database_username' => '资料库使用者',
+    'database_password' => '资料库密码',
+    'database_name' => '资料库名称',
+    'nameless_path' => 'Installation Path',
+    'nameless_path_info' => 'This is the path Nameless is installed in, relative to your domain. For example, if Nameless is installed at example.com/forum, this needs to be <strong>forum</strong>. Leave empty if Nameless is not in a subfolder.',
+    'friendly_urls' => 'Friendly URLs',
+    'friendly_urls_info' => 'Friendly URLs will improve the readability of URLs in your browser.<br />For example: <br />example.com/index.php?route=/forum<br />would become<br />example.com/forum.<br /><strong>Important!</strong><br />Your server must be configured correctly for this to work. You can see whether you can enable this option by clicking <a href=\'./rewrite_test\' target=\'_blank\'>here</a>.',
+    'enabled' => 'Enabled',
+    'disabled' => 'Disabled',
+    'character_set' => '字符集',
+    'database_engine' => 'Database Storage Engine',
+    'host' => 'Hostname',
+    'host_help' => 'The hostname is the <strong>base URL</strong> for your website. Do not include the subfolders from the Installation Path field, or http(s):// here!',
+    'database_error' => '请确认所有资料都已填写.',
+    'submit' => '送出',
+    'installer_now_initialising_database' => '安装程式正在初始化...请喝杯茶稍等',
+    'configuration' => '设定',
+    'configuration_info' => '请输入网站基本资料，这些设定在管理员后台可以看到.',
+    'configuration_error' => '网站名称字元限制 1~32，电子邮件字元限制 4~64.',
+    'site_name' => '网站名称',
+    'contact_email' => '联络电子邮件',
+    'outgoing_email' => '传出电子邮件',
+    'initialising_database_and_cache' => '初始化资料库与缓存...',
+    'unable_to_login' => '无法登入.',
+    'unable_to_create_account' => '无法建立帐号',
+    'input_required' => '请输入帐号、电子邮件和密码.',
+    'input_minimum' => '帐号最低限制 3 字元，电子邮件最低限制 4 字元，密码最低限制 6 字元.',
+    'input_maximum' => '帐号最高限制 20 字元，电子邮件最高限制 20 字元，密码最高限制 64 字元.',
+    'email_invalid' => 'Your email is not valid.',
+    'passwords_must_match' => '密码不相同.',
+    'creating_admin_account' => '建立管理员帐号',
+    'enter_admin_details' => '请输入管理员的帐号资料.',
+    'username' => '帐号 (Minecraft Username)',
+    'email_address' => '电子邮件',
+    'password' => '密码',
+    'confirm_password' => '确认密码',
+    'upgrade' => '升级',
+    'input_v1_details' => '请输入 Nameless version 1 的资料库资料.',
+    'installer_upgrading_database' => '请等待安装程序升级资料库...',
+    'errors_logged' => '错误已记录，点击继续继续升级.',
+    'continue' => '继续',
+    'convert' => '转换',
+    'convert_message' => '最后，您想要从不同的论坛升级?',
+    'yes' => '好',
+    'no' => '不了',
+    'converter' => 'Converter',
+    'back' => 'Back',
+    'unable_to_load_converter' => 'Unable to load converter!',
+    'finish' => '完成',
+    'finish_message' => '感谢安装 NamelessMC! 你现在可以造访管理员后台，在后台进一步设定网站.',
+    'support_message' => '如果需要支援，逛逛我们的 <a href="https://namelessmc.com" target="_blank">网站</a>, 或是加入我们的 <a href="https://discord.gg/9vk93VR" target="_blank">Discord伺服器</a> 或是 <a href="https://github.com/NamelessMC/Nameless/" target="_blank">GitHub 知识库</a>.',
+    'credits' => '贡献归功于',
+    'credits_message' => '从 2014年开始 非常感谢所有 <a href="https://github.com/NamelessMC/Nameless#full-contributor-list" target="_blank">NamelessMC 贡献者</a>'
+);
